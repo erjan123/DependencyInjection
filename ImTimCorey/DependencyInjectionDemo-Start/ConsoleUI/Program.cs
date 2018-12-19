cba56953792a3d60017fd76f17eda4903b114e1c
erjan123 <erjan123@yahoo.com>
2018-12-19 20:51:07 +0200

    Add project files.

---
+++ ImTimCorey/DependencyInjectionDemo-Start/ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessLogic businessLogic = new BusinessLogic();
+
+            businessLogic.ProcessData();
+
+            Console.ReadLine();
+        }
+    }
+}
